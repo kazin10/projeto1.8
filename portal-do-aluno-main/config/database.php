978607b5748fd4484e1a0271aac5ab2de60946c0
kazin10 <kazinsantos10@gmail.com>
2025-04-14 20:28:45 -0300

    Delete portal-do-aluno-main directory

--- portal-do-aluno-main/config/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'database' => __DIR__.'/../database/database.sqlite',
-    'options' => [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]
-    ];
--- portal-do-aluno-main/portal-professor/observaçoes.php
+++
@@ -1,7 +0,0 @@
-<form action="processar_observacao.php" method="post">
-    <textarea name="observacao" required></textarea>
-    <select name="aluno_id">
-        <!-- Lista alunos similar ao exemplo de faltas -->
-    </select>
-    <button>Enviar</button>
-</form>
